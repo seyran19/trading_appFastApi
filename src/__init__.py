fb6e8b2b50655cd8efa42722698ed2c8552d44ca
seyran <seyran.work@mail.com>
2024-04-25 20:56:36 +0300

    begin

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.auth.auth import auth_backend
-from src.auth.database import User
-from src.auth.manager import get_user_manager
-from src.auth.schemas import UserRead, UserCreate
-from src.operations.router import router as router_operation
-
